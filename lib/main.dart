0708f9bd4267c083289f385ea1deb952edec7845
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-05 16:10:41 +0530

    Stateless widget and hot reload end

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Qiktech.Dev'),
-          // centerTitle: true,
-          backgroundColor: Colors.black,
-        ),
-        body: Center(
-          child: Text('hey',
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Qiktech.Dev'),
+        // centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Text('hey ',
           style: GoogleFonts.indieFlower(
-            textStyle: TextStyle( color: Colors.amber)
+              textStyle: TextStyle( color: Colors.amber)
           ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text('click'),
-          backgroundColor: Colors.black,
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('click'),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
