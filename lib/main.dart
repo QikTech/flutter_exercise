996ab7580c668aeeb51912b0274f9575fdd7c13d
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-05 17:03:39 +0530

    Images - Assets and Network Both
    Image.asset('assets/img.jpeg'),
    
                Image.network('https://cdn.dribbble.com/users/1615584/screenshots/15976393/media/2817662416918f1d0c101fabacce1480.jpg?compress=1&resize=1200x900')

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Qiktech.Dev'),
-        // centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: Image.network('https://cdn.dribbble.com/users/1615584/screenshots/15976393/media/2817662416918f1d0c101fabacce1480.jpg?compress=1&resize=1200x900'),
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Text('click'),
-        backgroundColor: Colors.black,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Qiktech.Dev'),
+          // centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            Image.asset('assets/img.jpeg'),
+            Text('Qiktech'),
+            Image.network('https://cdn.dribbble.com/users/1615584/screenshots/15976393/media/2817662416918f1d0c101fabacce1480.jpg?compress=1&resize=1200x900')
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text('click'),
+          backgroundColor: Colors.black,
+        ));
   }
 }
