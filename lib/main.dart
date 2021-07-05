bec06515d828af7dfce4b01407ee64293ffa19ca
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-05 16:01:41 +0530

    Colors & Fonts End

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Qiktech.Dev'),
-          centerTitle: true,
+          // centerTitle: true,
+          backgroundColor: Colors.black,
         ),
         body: Center(
-          child: Text('hey'),
+          child: Text('hey',
+          style: GoogleFonts.indieFlower(
+            textStyle: TextStyle( color: Colors.amber)
+          ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: null,
           child: Text('click'),
-          
+          backgroundColor: Colors.black,
         ),
       ),
     ));
