8d85617af4a4dd76ff83fd9593ede9abbe62054a
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-11-14 17:49:23 +0530

    0.3

---
+++ lib/pages/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Home page this is'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
